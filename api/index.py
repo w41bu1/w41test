4378fc1dc6f3e38e15edede99213c94ffd760199
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:33:03 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -2,18 +2,6 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/svg')
-def about():
-    svg = """<?xml version="1.0" encoding="UTF-8"?>
-    <svg xmlns="http://www.w3.org/2000/svg">
-        <script>alert(document.domain)</script>
-    </svg>"""
-    return Response(svg, mimetype="image/svg+xml")
-
 @app.route("/sitemap.xml")
 def sitemap():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
@@ -28,5 +16,10 @@ def sitemap():
         <loc>http://localhost:5000/page3</loc>
     </url>
 </urlset>"""
-    return Response(xml_content.encode("utf-8"), mimetype="application/xml")
 
+    # Trả về bytes UTF-8, KHÔNG BOM
+    return Response(
+        xml_content.encode("utf-8"),
+        mimetype="application/xml",
+        headers={"Content-Type": "application/xml; charset=utf-8"}
+    )
