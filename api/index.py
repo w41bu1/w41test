020659b2016678f3f7c0fe6c8b83082a590820bd
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:30:34 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -17,17 +17,15 @@ def about():
 @app.route("/sitemap.xml")
 def sitemap():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE foo [ <!ENTITY xxe SYSTEM "file:///etc/passwd"> ]>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>http://localhost:5000/page1</loc>
-        </url>
-        <url>
-            <loc>http://localhost:5000/page2</loc>
-        </url>
-        <url>
-            <loc>http://localhost:5000/page3</loc>
-        </url>
-    </urlset>"""
-
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>http://localhost:5000/page1</loc>
+    </url>
+    <url>
+        <loc>http://localhost:5000/page2</loc>
+    </url>
+    <url>
+        <loc>http://localhost:5000/page3</loc>
+    </url>
+</urlset>"""
     return Response(xml_content, mimetype="application/xml")
