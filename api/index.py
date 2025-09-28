a1585df922f56863071f0fc0534664f7d525f585
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:40:52 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -1,39 +1,26 @@
-from flask import Flask, Response
+from fastapi import FastAPI, Response
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
+@app.get("/")
 def home():
-    return 'Hello, World!'
+    return {"message": "Hello, World!"}
 
-@app.route('/svg')
-def about():
-    svg = """<?xml version="1.0" encoding="UTF-8"?>
-    <svg xmlns="http://www.w3.org/2000/svg">
-        <script>alert(document.domain)</script>
-    </svg>"""
-    return Response(svg, mimetype="image/svg+xml")
-
-@app.route('/sitemap.xml')
+@app.get("/sitemap.xml", response_class=Response)
 def sitemap():
-    sitemap_xml = (
-        '<?xml version="1.0" encoding="UTF-8"?>\n'
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-        '  <url>\n'
-        '    <loc>http://localhost:5000/</loc>\n'
-        '    <lastmod>2025-09-29</lastmod>\n'
-        '    <changefreq>daily</changefreq>\n'
-        '    <priority>1.0</priority>\n'
-        '  </url>\n'
-        '  <url>\n'
-        '    <loc>http://localhost:5000/svg</loc>\n'
-        '    <lastmod>2025-09-29</lastmod>\n'
-        '    <changefreq>weekly</changefreq>\n'
-        '    <priority>0.8</priority>\n'
-        '  </url>\n'
-        '</urlset>'
-    )
-    return Response(sitemap_xml, mimetype="application/xml")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://w41test.vercel.app/</loc>
+    <lastmod>2025-09-29</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://w41test.vercel.app/svg</loc>
+    <lastmod>2025-09-29</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>"""
+    return Response(content=sitemap_xml, media_type="application/xml")
