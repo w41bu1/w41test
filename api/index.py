a21f0156e209f41974cdb0ce21f28ae9a08430b9
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:35:35 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -2,24 +2,36 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route("/sitemap.xml")
-def sitemap():
-    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>http://localhost:5000/page1</loc>
-    </url>
-    <url>
-        <loc>http://localhost:5000/page2</loc>
-    </url>
-    <url>
-        <loc>http://localhost:5000/page3</loc>
-    </url>
-</urlset>"""
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-    # Trả về bytes UTF-8, KHÔNG BOM
-    return Response(
-        xml_content.encode("utf-8"),
-        mimetype="application/xml",
-        headers={"Content-Type": "application/xml; charset=utf-8"}
-    )
+@app.route('/svg')
+def about():
+    svg = """<?xml version="1.0" encoding="UTF-8"?>
+    <svg xmlns="http://www.w3.org/2000/svg">
+        <script>alert(document.domain)</script>
+    </svg>"""
+    return Response(svg, mimetype="image/svg+xml")
+
+@app.route('/sitemap.xml')
+def sitemap():
+    sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>http://localhost:5000/</loc>
+            <lastmod>2025-09-29</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>http://localhost:5000/svg</loc>
+            <lastmod>2025-09-29</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    </urlset>"""
+    return Response(sitemap_xml, mimetype="application/xml")
+
+if __name__ == '__main__':
+    app.run(debug=True)
